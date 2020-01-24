82475f32885861fccba2629dcd54d196f9f6f4c6
Robert A <daspanisharmada@gmail.com>
2020-01-24 08:10:20 -0800

    Sign In Code

---
+++ code/App.tsx
@@ -0,0 +1,88 @@
+// import * as React from "react"
+// import { Override, Data } from "framer"
+
+// const appState = Data({
+//     progress: 0,
+// })
+
+// export function ProgressBar(): Override {
+//     return {
+//         progress: appState.progress,
+//     }
+// }
+
+// export function Button(): Override {
+//     return {
+//         onTap: () => {
+//             appState.progress = 100
+//         },
+//     }
+// }
+
+import { Override, Data } from "framer"
+import * as React from "react"
+
+const appState = Data({
+    emailError: "BLANK_ERROR",
+    passwordError: "BLANK_INPUT",
+})
+
+export function EmailInput(): Override {
+    return {
+        onValueChange: (value: string) => {
+            console.log(value.includes("@"))
+
+            appState.emailError =
+                value.length === 0
+                    ? "BLANK_INPUT"
+                    : !value.includes("@") || !value.includes(".com")
+                    ? "WRONG_INPUT"
+                    : ""
+        },
+    }
+}
+
+export function PasswordInput(): Override {
+    return {
+        onValueChange: (value: string) => {
+            console.log(value.includes("@"))
+
+            appState.emailError =
+                value.length === 0
+                    ? "BLANK_INPUT"
+                    : value.length < 5
+                    ? "WRONG_INPUT"
+                    : ""
+        },
+    }
+}
+
+export function EmailError(): Override {
+    const errorMessages = {
+        BLANK_INPUT: "Enter your email address",
+        WRONG_FORMAT: "Enter a valid email address",
+    }
+
+    return {
+        visible: appState.emailError !== "",
+        text: errorMessages[appState.emailError],
+    }
+}
+
+export function PasswordError(): Override {
+    const errorMessages = {
+        BLANK_INPUT: "Enter your password",
+        WRONG_FORMAT: "Your password is too short",
+    }
+
+    return {
+        visible: appState.passwordError !== "",
+        text: errorMessages[appState.passwordError],
+    }
+}
+
+export function ContinueButton(): Override {
+    return {
+        disabled: appState.emailError || appState.passwordError,
+    }
+}
--- code/ProgressBar.tsx
+++ code/ProgressBar.tsx
@@ -1,7 +1,8 @@
 import * as React from "react"
-import { Frame } from "framer"
+import { Frame, addPropertyControls, ControlType } from "framer"
 
-export function ProgressBar() {
+export function ProgressBar(props) {
+    const { progress, duration } = props
     return (
         <Frame
             height={44}
@@ -12,7 +13,37 @@ export function ProgressBar() {
             overflow="hidden"
             border="4px solid #dedede"
         >
-            <Frame height="100%" background="0099FF" />
+            <Frame
+                height="100%"
+                background="0099FF"
+                initial={{ width: "100%" }}
+                animate={{ width: progress + "%" }}
+                transition={{ ease: "linear", duration }}
+            />
         </Frame>
     )
 }
+
+ProgressBar.defaultProps = {
+    progress: 100,
+    duration: 2,
+}
+
+addPropertyControls(ProgressBar, {
+    progress: {
+        title: "Progress",
+        type: ControlType.Number,
+        min: 0,
+        max: 100,
+        step: 5,
+        defaultValue: 0,
+    },
+    duration: {
+        title: "Duration",
+        type: ControlType.Number,
+        min: 0,
+        max: 100,
+        step: 0.5,
+        defaultValue: 2.5,
+    },
+})
